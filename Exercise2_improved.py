eaeda7054fbd259428b8e2c603df7df9bcfd0131
Arthur <arthurpcanuto@gmail.com>
2023-03-22 01:30:20 -0300

    Fixed some grammar and typos.

--- Exercise2_improved.py
+++ Exercise2_improved.py
@@ -1,16 +1,16 @@
-#We ask user how to input the total hours user works and
-#how much user gets paid per hour. Then we float those inputs.
+# We ask user to input the total hours user works and
+# how much user gets paid per hour. Then we float those inputs.
 total_hours = float(input('How many hours do you work? '))
 pay = float(input('How much do you get paid per hour? '))
 
-#Here we define how many extra hours beyond 40 does the user work.
+# Here we define how many extra hours beyond 40 the user works
 if total_hours > 40:
     extra_hours = total_hours - 40
 elif total_hours <= 40:
     extra_hours = 0
 
-#Here we calculate the user's total pay taking into consideration that
-#every hour above 40 has extra pay.
+# Here we calculate the user's total pay taking into consideration that
+# every hour above 40 has extra pay.
 total_pay = (40 * pay) + (extra_hours * (1.5*pay))
 
 print('You get paid this much:', total_pay)
