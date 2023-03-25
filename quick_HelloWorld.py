3d853aa0ea5f6ceb45ac3af5b7c607a289d74389
Arthur <arthurpcanuto@gmail.com>
2023-03-25 01:01:13 -0300

    Fixed typos.

--- quick_HelloWorld.py
+++ quick_HelloWorld.py
@@ -1,4 +1,4 @@
-#apprently writing on of those is a prerequisite to calling yourself a programmer so here it goes.
+#Apprently writing one of these is a prerequisite to calling yourself a programmer so here it goes.
 print("Hello World!")
 name = input("What is your name? ")
 print("Hello, " + name + "!")
