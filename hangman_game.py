030f177a1e939025ff4fce8ca6c201d18bd57184
Arthur <arthurpcanuto@gmail.com>
2023-03-25 02:47:25 -0300

    Still work in progress, but I did manage one of the functions.

---
+++ hangman_game.py
@@ -0,0 +1,47 @@
+import random
+
+#This function check to see if the user input a letter, if user didn't it prompts user to do so.
+def inputcheck(letter):
+    while not letter.isalpha():
+        print('\nPlease enter a valid letter.')
+        letter = input('Please enter a letter: ')
+    return letter
+
+tries = 6
+Playing = True
+
+
+#Initial message.
+print('Welcome to my hangman game, it will generate a random word from a dictionary and I hope you know how'
+      ' to play hangman because I do not wnat to explain here!')
+
+input('\nPress enter to start!')
+
+#This is the list of words for the game.
+words = ['about', 'after', 'again', 'airplane', 'almost', 'already', 'always', 'another', 'answer', 'anyone', 'anything', 'around', 'beautiful', 'because', 'before', 'behind', 'believe', 'between', 'bicycle', 'billion',
+         'business', 'calendar', 'camera', 'capital', 'careful', 'carrot', 'center', 'century', 'certain', 'chance', 'change', 'check', 'chicken', 'church', 'circle', 'citizen', 'coffee', 'college', 'company', 'computer',
+         'consider', 'continue', 'control', 'correct', 'country', 'courage', 'create', 'cultural', 'customer', 'danger', 'daughter', 'decide', 'defense', 'delight', 'democracy', 'develop', 'diamond', 'difference', 'difficult',
+         'dinner', 'direction', 'discover', 'distance', 'diverse', 'doctor', 'document', 'domestic', 'doorway', 'downtown', 'dragon', 'dramatic', 'economy', 'education', 'election', 'electric', 'element', 'elevator', 'embrace',
+         'emotion', 'employee', 'engine', 'enough', 'enter', 'entire', 'environment', 'equal', 'escape', 'especially', 'establish', 'event', 'everybody', 'everyone', 'everything', 'everywhere', 'exactly', 'excellent', 'exciting',
+         'exercise', 'existing', 'expensive', 'experience', 'explain', 'expression', 'familiar', 'family', 'famous', 'favorite', 'federal', 'fifteen']
+
+
+
+while Playing is True:
+    #Here we randomly select a word from the list.
+    random_word = random.choice(words)
+    #Here we count how many letters the word has.
+    letter_count = int(len(random_word))
+
+    for i in range(letter_count):
+        print('_ ', end="")
+
+    letter = input('\nPlease enter a letter: ')
+    letter = inputcheck(letter)
+
+    stop = input('\nY to stop: ')
+
+    if stop == "Y":
+        Playing = False
+
+
