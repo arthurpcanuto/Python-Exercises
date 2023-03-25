e775fa851ab15f58c83ff8bb666428a5a580a13b
Arthur <arthurpcanuto@gmail.com>
2023-03-24 23:40:28 -0300

    Taking care of this now.

---
+++ quick_HelloWorld.py
@@ -0,0 +1,4 @@
+#apprently writing on of those is a prerequisite to calling yourself a programmer so here it goes.
+print("Hello World!")
+name = input("What is your name? ")
+print("Hello, " + name + "!")
