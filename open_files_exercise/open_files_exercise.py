976564f327d4e452b9dc03eaf5bfe87377313b62
Arthur <arthurpcanuto@gmail.com>
2023-03-27 00:34:04 -0300

    Forgot to format the code on the last push.

--- open_files_exercise/open_files_exercise.py
+++ open_files_exercise/open_files_exercise.py
@@ -1,17 +1,18 @@
-#We use input() to get the file name of the file user wants to read through.
+# We use input() to get the file name of the file user wants to read through.
 file_name = input("Enter a file name: ")
 
 
-#We use open() to assign this variable the file handle, hence why it is called (fhand).
+# We use open() to assign this variable the file handle, hence why it is called (fhand).
 fhand = open(file_name)
 
 
 for line in fhand:
-    #This removes the extra spaces on the end of the lines.
+    # This removes the extra spaces on the end of the lines.
     line = line.rstrip()
-    #If the line does not start with 'From' the loop skips to its next iteration.
-    if not line.startswith("From ") : continue
-    #This splits the string (line) everytime there is a space(' '), and makes a list out of the words in that line.
+    # If the line does not start with 'From' the loop skips to its next iteration.
+    if not line.startswith("From "):
+        continue
+    # This splits the string (line) everytime there is a space(' '), and makes a list out of the words in that line.
     words = line.split()
-    #This prints the third word stored in the variable (words).
+    # This prints the third word stored in the variable (words).
     print(words[2])
