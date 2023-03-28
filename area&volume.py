9db4d9c8b6c765abaa9ec85c5ca543bc755d8e4a
Arthur <arthurpcanuto@gmail.com>
2023-03-28 01:46:41 -0300

    Improved version, I think.

--- area&volume.py
+++ area&volume.py
@@ -1,3 +1,4 @@
+from decimal import Decimal
 
 
 #This function will return the area.
@@ -16,45 +17,56 @@ def volume(length, width, height):
 def surface_area(legnth, width, height):
     return (legnth * width * 2) + (legnth * height * 2) + (width * height * 2)
 
-#This function will capture the user's input and return them as float numbers
-#So now we can use them to calculate our result.
+#This function will check if the user input is valid.
+def check_Decimal_input(prompt):
+    while True:
+        try:
+            value = Decimal(input(prompt))
+            return value
+        except ValueError:
+            print("Please input a valid number.")
+
+#This function will capture the user's input and return them as decimal numbers
+#So now we can use them to calculate our result with precision.
 def values(choice):
-    value_length = float(input("What is the length of your shape? "))
-    value_width = float(input("What is the width of your shape? "))
+    #We call our function (check_Decimal_input) to check if user input is the correct type.
+    value_length = check_Decimal_input("What is the length of your shape? ")
+    value_width = check_Decimal_input("What is the width of your shape? ")
     
     if choice == "area" or choice == "perimeter":
         return value_length, value_width
     
-    elif choice == "volume" or choice == "surface area":
-        value_height = float(input("What is the height of your shape?"))
+    elif choice == "volume" or choice == "surface area" or choice == "surface":
+        value_height = check_Decimal_input("What is the height of your shape? ")
         return value_length, value_width, value_height
+    
 
 #This function will take care of calculating our result.
 def result(choice, returned_values):
     if choice == "area":
         area_result = area(*returned_values)
-        print(area_result)
+        print("\nThe area of your shape is:", area_result)
     elif choice == "perimeter":
         perimeter_result = perimeter(*returned_values)
-        print(perimeter_result)
+        print("\nThe perimeter of you shape is:", perimeter_result)
     elif choice == 'volume':
         volume_result = volume(*returned_values)
-        print(volume_result)
+        print("\nThe volume of your shape is:", volume_result)
     else:
         surfaceArea_result = surface_area(*returned_values)
-        print(surfaceArea_result)
+        print("\nThe surface area of you shape is:", surfaceArea_result)
 
 
 
 
-
+#Our initial message to the user.
 print("\nThis program calculates the area, volume, perimeter and surface area of the "\
 "retangles and retangular prisms. Yes, it does include cubes.")
 
 #This is our main function and the program's logic.
 def main():
     #This variable defines the valid inputs for user.
-    valid_choices = ["area", "perimeter", "volume", "surface area"]
+    valid_choices = ["area", "perimeter", "volume", "surface area", "surface"]
     
     #This loop checks if user's choice is valid, if it isn't it prompts user to choose again.
     while True:
@@ -67,12 +79,25 @@ def main():
         else:
             print("Sorry, " + choice + " is not a valid choice. Please try again.")
     
+    #We store the converted values from user input in this variable.
     returned_values = values(choice)
     
+    #We call our result function to calculate based on user's choice and chosen values.
     result(choice, returned_values)
 
-            
-main()
+
+
+
+#Here we define a control function and initialize our main loop.
+Calculating = True
+while Calculating is True:            
+    main()
+
+    user_decision = input("Would you like to calculate something else? Enter 'yes' if yes. ")
+
+    if user_decision.lower() != 'yes':
+        Calculating = False
+
         
 
 
